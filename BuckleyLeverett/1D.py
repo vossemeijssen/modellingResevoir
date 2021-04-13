291769ca9c40c8eeef8261d9684f4d4dc0bf7188
Unknown <vossemeijssen@hotmail.com>
2021-04-13 12:00:41 +0200

    Variabelen toegevoegd aan BL1D

--- BuckleyLeverett/1D.py
+++ BuckleyLeverett/1D.py
@@ -1,6 +1,18 @@
-import numpy as np
 
 
 # Variables
-u_inj = 1
+u_inj = 1.0  # Water injection speed
+S_or = 0.2  # Oil rest-saturation
+S_wc = 0.2  # Water capillary saturation
+L = 10.0  # Total length
+dx = 0.1  # distance step
+dt = 1.0  # time step
+phi = 0.2  # Porosity
 
+
+def D_cap(S_w):
+    return 0
+
+
+def f_w(S_w):
+    return

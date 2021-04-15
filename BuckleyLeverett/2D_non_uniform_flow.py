bfd655bf67ce89c216ec02c602d1205d9fe0a935
Kevin Dijkstra <kevin.w.dijkstra@gmail.com>
2021-04-15 17:27:46 +0200

    added some stuff for 2D correct

--- BuckleyLeverett/2D_non_uniform_flow.py
+++ BuckleyLeverett/2D_non_uniform_flow.py
@@ -2,8 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import tqdm
 from reservoirModule import *
+from scipy.sparse import  diags
 
-dx = 0.1
+dx = 5
 L  = 10
 W  = 10
 
@@ -18,7 +19,9 @@ c = Constants(
     n_w = 4,  # >=1
     n_o = 2,  # >=1
     S_or = 0.1,  # Oil rest-saturation
-    S_wc = 0.1 )
+    S_wc = 0.1,
+    sigma = 1,
+    labda = 1)
 
 
 N = int(L/dx)+1
@@ -28,12 +31,39 @@ Sw = np.zeros(N*M)
 p  = np.zeros(N*M)
 
 dl_tot = np.zeros(N*M)
+dl_totdx = np.zeros(N*M)
+dl_totdy = np.zeros(N*M)
 
-for i in range(N*M):
-    if i%
-    xp1 = i + 1
-    xm1
-    dl_tot = dl_w(Sw[i],c)+dl_o(Sw[i],c)
+K = N*M
+for i in range(K):
+    dl_tot[i] = dl_w(Sw[i],c)+dl_o(Sw[i],c)
+for i in range(K):
+    dl_totdx[i] = 1 / 2 / dx * (dl_tot[(i + 1    ) % K] - dl_tot[(i - 1    ) % K])
+    dl_totdy[i] = 1 / 2 / dx * (dl_tot[(i + N + 1) % K] - dl_tot[(i - N - 1) % K])
 
+
+
+diagonals = [-4/dx/dx*dl_tot,
+             dl_tot[0:-1]/dx/dx+dl_totdx[0:-1]/2/dx,
+             dl_tot[0:-1]/dx/dx-dl_totdx[0:-1]/2/dx,
+             dl_tot[0:-N]/dx/dx+dl_totdy[0:-N]/2/dx,
+             dl_tot[0:-N]/dx/dx-dl_totdy[0:-N]/2/dx]
+D = diags(diagonals, [0,1,-1,N,-N]).toarray()
+
+list = []
+for i in range(K):
+    if i%N == 0:
+        list.append(i)
+    if i%N == N-1:
+        list.append(i)
+    # if i/N == 0:
+    #     list.append(i)
+    # if i/N == N-1:
+    #     list.append(i)
+
+D = np.delete(D,list,axis=1)
+D = np.delete(D,list,axis=0)
+print(D)
+# plt.show(?)
 
 

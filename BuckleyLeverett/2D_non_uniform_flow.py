3f9747ba67a0a2473e63c158fd3705d5a7cc276d
Kevin Dijkstra <kevin.w.dijkstra@gmail.com>
2021-04-15 21:09:09 +0200

    added pressure plot

--- BuckleyLeverett/2D_non_uniform_flow.py
+++ BuckleyLeverett/2D_non_uniform_flow.py
@@ -5,9 +5,9 @@ import math
 from reservoirModule import *
 from scipy.sparse import  diags
 
-dx = 5
-L  = 15
-W  = 15
+dx = 0.2
+L  = 6
+W  = 10
 
 c = Constants(
     phi = 0.1,  # Porosity
@@ -27,53 +27,67 @@ c = Constants(
 
 N = int(L/dx)+1
 M = int(W/dx)+1
+# print("N = ",N)
+# print("M = ",M)
 
-Sw = np.random.rand(N*M)
-print(Sw)
+
+Sw = c.S_wc*np.ones(N*M)
+Sw[0:N] = 1-c.S_or
+# print(Sw.reshape(M,N))
 p  = np.zeros(N*M)
 
+l_tot  = np.zeros(N*M)
 dl_tot = np.zeros(N*M)
 dl_totdx = np.zeros(N*M)
 dl_totdy = np.zeros(N*M)
 
 K = N*M
 for i in range(K):
+    l_tot[i]  = l_o(Sw[i],c)
     dl_tot[i] = dl_w(Sw[i],c)+dl_o(Sw[i],c)
 for i in range(K):
-    dl_totdx[i] = 1 / 2 / dx * (dl_tot[(i + 1    ) % K] - dl_tot[(i - 1    ) % K])
-    dl_totdy[i] = 1 / 2 / dx * (dl_tot[(i + N + 1) % K] - dl_tot[(i - N - 1) % K])
+    dl_totdx[i] = 1 / 2 / dx * (dl_tot[(i + 1) % K] - dl_tot[(i - 1) % K])
+    dl_totdy[i] = 1 / 2 / dx * (dl_tot[(i + N) % K] - dl_tot[(i - N) % K])
 
 
 
-diagonals = [-4/dx/dx*dl_tot,
-             dl_tot[0:-1]/dx/dx+dl_totdx[0:-1]/2/dx,
-             dl_tot[0:-1]/dx/dx-dl_totdx[0:-1]/2/dx,
-             dl_tot[0:-N]/dx/dx+dl_totdy[0:-N]/2/dx,
-             dl_tot[0:-N]/dx/dx-dl_totdy[0:-N]/2/dx]
+diagonals = [-4/dx/dx*l_tot,
+             l_tot[0:-1]/dx/dx+dl_totdx[0:-1]/2/dx,
+             l_tot[0:-1]/dx/dx-dl_totdx[0:-1]/2/dx,
+             l_tot[0:-N]/dx/dx+dl_totdy[0:-N]/2/dx,
+             l_tot[0:-N]/dx/dx-dl_totdy[0:-N]/2/dx]
 D = diags(diagonals, [0,1,-1,N,-N]).toarray()
 
 list = []
 for i in range(K):
     if i%N == 0:
         list.append(i)
-    if i%N == N-1:
+    elif i%N == N-1:
         list.append(i)
-    if math.floor(i/N) == 0:
+    elif math.floor(i/N) == 0:
         list.append(i)
-    if math.floor(i/N) == N-1:
+    elif math.floor(i/N) == M-1:
         list.append(i)
+
+# print(np.shape(D))
+# print(list)
 
 D = np.delete(D,list,axis=1)
 D = np.delete(D,list,axis=0)
+# print(np.shape(D))
 l = np.ones((N-2)*(M-2))
 l[-1] = 1
 D = np.vstack([D,l])
 # print(D)
 l = np.zeros(((N-2)*(M-2))+1)
-l[-1] = 1
+l[-1] = 10
 
 p = np.linalg.lstsq(D,l, rcond=None)
 p = p[0]
+# print(p.reshape(M-2,N-2))
 
+plt.contourf(p.reshape(M-2,N-2))
+plt.matshow(Sw.reshape(M,N))
+plt.show()
 
 
